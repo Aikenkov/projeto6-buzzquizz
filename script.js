0472795ab83cb57d6362245c83f3da91c782d16d
MateusDiniz <mateus.090998@gmail.com>
2022-06-29 16:28:55 -0300

    renderizar + começo da criaçao + levels done

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ let content = document.querySelector(".content");
 
 //setInterval(findQuizzes, 5000);
 
+
+//parte para Renderizar tela inicial e ir para o quizz clicado
+
 function renderQuizzesTela1(array){
     content.innerHTML = "";
     quizzes = array.data; 
@@ -85,6 +88,14 @@ function findQuizzes(){
    promise.then(renderQuizzesTela1);
 }
 
+function goToQuizz(){
+    content.innerHTML= "TAMO FAZENDO"
+}
+
+//Fim da parte para Renderizar tela inicial e ir para o quizz clicado
+
+//Começo da parte para criar Quizz
+
 function createQuizz(){
     content.innerHTML = ''
     content.innerHTML += `
@@ -108,19 +119,69 @@ function createQuizz(){
 
 }
 
-function goToQuizz(){
-    content.innerHTML= "TAMO FAZENDO"
+//Parte para verificar os niveis do quizz
+
+function abrirLevels(elemento){
+    elemento.parentNode.parentNode.classList.toggle("fechada")
+    elemento.classList.add("hidden");
 }
 
 function createQuizzLevels(qnt){
+    content.innerHTML= "";
+    content.innerHTML+= `<h2>Agora, Decida os níveis</h2>`
     for(let i = 0; i < qnt; i++){
-
+        content.innerHTML+=`
+        <div class="levels-box fechada" >
+            <h3>Nível ${i} <ion-icon name="create-outline" onclick="abrirLevels(this)"></ion-icon></h3>
+            <textarea name="titulo" class="titulo" placeholder="Título do nível"></textarea>
+            <textarea name="acerto" class="acerto" placeholder="% de acerto mínima"></textarea>
+            <textarea name="URL" type="url" class="url" placeholder="URL da imagem do nível"></textarea>
+            <textarea name="Descrição" class="descricao" placeholder="Descrição do nível"></textarea>
+        </div>
+        `
     }
+    content.innerHTML+=` 
+            <div class="redButton1" onclick="verifyFinishQuiz(qnt)">
+                <p>Finalizar Quizz</p>
+            </div>
+    `
 }
 
+function verifyFinishQuizz(qnt){
+    let arrayLevels = [
+        {
+            titulo: '',
+            acerto: '',
+            URL: '',
+            descricao: ''
+        }
+    ];
+    let titulos = document.querySelectorAll("textarea.titulo").value;
+    let acertos = document.querySelectorAll("textarea.acerto").value;
+    let urls = document.querySelectorAll("textarea.url").value;
+    let descricoes = document.querySelectorAll("textarea.descricao").value;
+    let flag = 0;
+    for(let i = 0; i < qnt; i++){
+        if(acertos[i] !== 0){
+            flag++
+            if(flag === qnt-1){
+                alert("Preencha os dados corretamente!!!")
+                createQuizzLevels(qnt);    
+            }
+        }
+        if(titulos[i].length < 10 || (acertos[i] < 0 && acertos[i] > 100) || descricoes[i].length < 30){
+            alert("Preencha os dados corretamente!!!")
+            createQuizzLevels(qnt);
+        }
+        arrayLevels[i].titulo = titulos[i];
+        arrayLevels[i].acerto = acertos[i];
+        arrayLevels[i].URL = urls[i];
+        arrayLevels[i].descricao = descricoes[i];
+    }
+    finishQuizz(arrayLevels);
+}
 
-
-
+//Finalizada parte para verificar niveis do quizz
 
 
 
