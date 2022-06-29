fa0a207ceaf3c8974e9f713b1ede39d46687589c
MateusDiniz <mateus.090998@gmail.com>
2022-06-29 12:43:32 -0300

    renderização pronta tela1

--- script.js
+++ script.js
@@ -2,28 +2,32 @@
 const url = "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes";
 let quizzes;
 let selfIds = [];
+let intervalRenderQuizz;
+let content = document.querySelector(".content");
 
+//setInterval(findQuizzes, 5000);
 
-function renderQuizzes(array){
-    quizzes = array.data;
-    let content = document.querySelector(".content")
-    content = "";
-
-    if(selfIds.length===0){
-        content.innerHTML+=`
+function renderQuizzesTela1(array){
+    content.innerHTML = "";
+    quizzes = array.data; 
+    console.log(quizzes);
+    if(selfIds.length === 0){ 
+        content.innerHTML += `
         <div class="my-quizzes-null">
             <span>
                 Você não criou nenhum<br>
                 quizz ainda :(
             </span>
-            <button>Criar Quizz</button>
+            <button onclick="createQuizz()">Criar Quizz</button>
         </div>    
         `
+        
         content.innerHTML+= `
         <h1>Todos os Quizzes</h1>
         <div class="all-quizzes">
         </div>
-        `
+        `;
+
         let allQuizzes = document.querySelector(".all-quizzes");                
         for(let i = 0; i < quizzes.length; i++){
             allQuizzes.innerHTML+= `
@@ -31,26 +35,61 @@ function renderQuizzes(array){
                 <img src="${quizzes[i].image}">
                 <span>${quizzes[i].title}</span>
             </div>         
-            `
+            `;
         }
+        
     }else{
-        content.innerHTML+= `<h1>Seus Quizzes<ion-icon name="add-circle"></ion-icon></h1>`
+        content.innerHTML+= ` 
+        <div class="title">
+            <span><h1>Seus Quizzes</h1></span><span><ion-icon name="add-circle"></ion-icon></span>
+        </div>
+
+        <div class="my-quizzes">
+        </div>    
+
+        <h1>Todos os Quizzes</h1>
+
+        <div class="all-quizzes">
+        </div>    
+        `;
+        let myQuizzes = document.querySelector(".my-quizzes");
+        let allQuizzes = document.querySelector(".all-quizzes");
+        let flag = 0;
         for(let i = 0; i < quizzes.length; i++){
+            flag = 0;
             for(let j = 0; j < selfIds.length; j++){
                 if(quizzes[i].id === selfIds[j]){
-                    
+                    myQuizzes.innerHTML+=`
+                        <div class="quizz">  
+                            <img src="${quizzes[j].image}">
+                            <span>${quizzes[j].title}</span>
+                        </div>         
+                    `;
+                    flag = 1;
                 }
+            }
+            if(flag === 0){
+                allQuizzes.innerHTML+= `
+                    <div class="quizz">  
+                        <img src="${quizzes[i].image}">
+                        <span>${quizzes[i].title}</span>
+                    </div>         
+                `;
             }
         }
     }
-
 }
 
 function findQuizzes(){
    const promise = axios.get(url);
-   promise.then(renderQuizzes);
+   promise.then(renderQuizzesTela1);
 }
 
+function createQuizz(){
+
+}
+
+findQuizzes();
 
 //Fim mateus
 
